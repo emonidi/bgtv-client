5572c2ce06948a1aeb3ab3bae9b59cb431344a15
Emiliyan Gospodinov <emonidi@gmail.com>
2017-04-02 14:01:33 +0200

    Video ads stable

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -16,46 +16,42 @@ export class VideoComponent implements OnInit, OnChanges {
   constructor() { }
 
   ngOnInit() {
-    this.videoPlayer = videojs('video', {
+    this.videoPlayer = videojs('content_video', {
       html5: {
         hls: {
           withCredentials: true,
-          overrideNative:true
+          overrideNative: true
         }
       }
-    }).ready(() => {
-      this.options = {
-        id: 'video',
-        // autoPlayAdBreaks:true,
-        locale: 'bg',
-        nativeControlsForTouch:false,
-        adsRenderingSettings: {
-          enablePreloading: true,
-          playAdsAfterTime: 30,
-          restoreCustomPlaybackStateOnAdBreakComplete: true
-        },
-        adTagUrl:   'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'
-      };
     })
-      .on('play', () => {
-        if (typeof this.videoPlayer.ima === 'object') {
-          this.videoPlayer.ima.initializeAdDisplayContainer();
-          this.videoPlayer.ima.requestAds();
-          this.videoPlayer.play();
-        } else {
-          this.videoPlayer.ima(this.options);
-          this.videoPlayer.ima.initializeAdDisplayContainer();
-          this.videoPlayer.ima.requestAds();
-          this.videoPlayer.play();
-        }
-         
-      });
+
+    this.options = {
+      id: 'content_video',
+      // autoPlayAdBreaks:true,
+      locale: 'bg',
+      debug: true,
+      nativeControlsForTouch: false,
+      adsRenderingSettings: {
+        enablePreloading: true,
+        playAdsAfterTime: 30,
+        restoreCustomPlaybackStateOnAdBreakComplete: true
+      },
+      adTagUrl: 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'
+    };
+    this.startEvent = 'click';
+    if (navigator.userAgent.match(/iPhone/i) ||
+      navigator.userAgent.match(/iPad/i) ||
+      navigator.userAgent.match(/Android/i)) {
+      this.startEvent = 'touchend';
+    }
 
 
-      this.videoPlayer.on('adserror',(err)=>{
-        console.log(err.data.AdError);
-        // alert(err.data.AdError);
-      });
+
+
+    this.videoPlayer.on('adserror', (err) => {
+      console.log(err.data.AdError);
+      // alert(err.data.AdError);
+    });
 
 
   }
@@ -70,6 +66,34 @@ export class VideoComponent implements OnInit, OnChanges {
         src: change.source.currentValue,
         type: 'application/x-mpegURL',
         withCredentials: true
+      })
+      this.videoPlayer.one(this.startEvent, function (ev) {
+        ev.preventDefault();
+        console.log(this.ima)
+        if (typeof this.ima !== 'object') {
+          this.ima({
+            id: 'content_video',
+            // autoPlayAdBreaks:true,
+            locale: 'bg',
+            debug: true,
+            nativeControlsForTouch: false,
+            adsRenderingSettings: {
+              enablePreloading: true,
+              playAdsAfterTime: 30,
+              restoreCustomPlaybackStateOnAdBreakComplete: true
+            },
+            adTagUrl: 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'
+          });
+          this.ima.requestAds();
+          this.ima.initializeAdDisplayContainer();
+
+          this.play();
+        }else{
+          this.ima.requestAds();
+          this.ima.initializeAdDisplayContainer();
+
+          this.play();
+        }
       })
 
     }
